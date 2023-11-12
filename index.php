04bc7875f2fb104ea63147c3c2ee6d4040fe3324
Patrick van Bergen <patrick.vanbergen@gmail.com>
2023-11-12 11:49:16 +0100

    picture

--- index.php
+++ index.php
@@ -4,18 +4,21 @@ $title = "Home";
 include 'template/header.php';
 ?>
 
-<section>
+<section class="home-section">
+    <img src="images/pattern-image.drawio.png" class="pattern-image" />
+
     <header class="content">
         <h1>About architectural patterns</h1>
     </header>
     <p>
-        In playing games, warfare, and management we can distinguish three levels of analysis: operational, tactical, and strategic. In software development we could associate these with <a href="https://en.wikipedia.org/wiki/Structured_programming">structured programming</a>, <a href="https://en.wikipedia.org/wiki/Software_design_pattern">design patterns</a>, and ... <a href="https://en.wikipedia.org/wiki/Architectural_pattern">architectural patterns</a>.
+        In playing games, management, and warfare we can distinguish three levels of analysis: operational, tactical, and strategic. In software development we could associate these with <a href="https://en.wikipedia.org/wiki/Structured_programming">structured programming</a>, <a href="https://en.wikipedia.org/wiki/Software_design_pattern">design patterns</a>, and ... <a href="https://en.wikipedia.org/wiki/Architectural_pattern">architectural patterns</a>.
     </p>
     <p>
         Most resources for programmers focus on programming, and a fair amount of information is available on design patterns. This is understandable as this is what we need most of the time. But there can be times that you need to know about architectural patterns as well.
     </p>
     <p>
-        I once found myself as a software developer in a position where I was asked to build a new application, a virtual driving instructor. I had no idea how to approach this problem. I could just start building classes and functions and hope for the best, but I felt I needed to have an idea of the big picture of the application before I endeavoured the project. If I had the structure wrong, the program could be be arduous to complete and it could become unnecessarily slow. Furthermore, I would have no way to communicate to others about the structure of the application. I needed an architecture, before I started building. But I didn't have any examples to base it upon.
+        I once found myself as a software developer in a position where I was asked to build a new application, a virtual driving instructor. I had no
+        idea how to approach this problem. I could just start building classes and functions and hope for the best, but I felt I needed to have an idea of the big picture of the application before I endeavoured the project. If I had the structure wrong, the program could be be arduous to complete and it could become unnecessarily slow. Furthermore, I would have no way to communicate to others about the structure of the application. I needed an architecture, before I started building. But I didn't have any examples to base it upon.
     <p>
         This site is intended to help you in this process. It serves as index to the most well known architectural patterns. They have been used for a wide variety of applications and serve many different purposes. The list continues to grow as new patterns are still being invented. They are meant as a source of inspiration and may be freely adapted and combined. The pages contain just a minimal amount of information to describe and evaluate the pattern. To learn more about them follow the links provides, or do a web-search.
     </p>
@@ -32,9 +35,10 @@ include 'template/header.php';
         Beauty is more important in computing than anywhere else in technology because software is so complicated. Beauty is the ultimate defense against complexity.
     </blockquote>
     <p>—David Gelernter (in Machine Beauty: Elegance and the Heart of Technology) Found <a target="_blank" href="https://vanderburg.org/etc/quotes/gelernter-beauty/">here</a>.
+    </p>
     <p>
         <center><img src="images/dragon.jpg" /></center>
-    <p>
+    </p>
 
 </section>
 
