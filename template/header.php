2e5d5d7ef0cdb207ed35f6aded796d678c1651da
Patrick van Bergen <patrick.vanbergen@gmail.com>
2023-12-02 11:56:34 +0100

    gtm

--- template/header.php
+++ template/header.php
@@ -18,6 +18,16 @@
 	</head>
 	<body class="is-preload">
 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4E7P326XB4"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4E7P326XB4');
+        </script>
+
 		<!-- Wrapper -->
 			<div id="wrapper">
 
