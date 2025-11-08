c60454dca42dbaadda32e651db23fe7fc88eefb1
Patrick van Bergen <patrick.vanbergen@gmail.com>
2025-11-08 11:53:47 +0100

    undefined $title

--- src/template/header.php
+++ src/template/header.php
@@ -6,7 +6,7 @@
 -->
 <html lang="en">
 	<head>
-		<title><?= $title ? $title : $pattern['name'] ?> | Architectural Patterns</title>
+		<title><?= !empty($title) ? $title : $pattern['name'] ?> | Architectural Patterns</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
